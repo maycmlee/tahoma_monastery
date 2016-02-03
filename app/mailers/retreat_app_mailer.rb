5b98908b9260c8891901371318ea8952e943425a
maycmlee <may.cm.lee@gmail.com>
2016-02-02 20:32:43 -0500

    wrote html and text for mail to be sent out

--- app/mailers/retreat_app_mailer.rb
+++ app/mailers/retreat_app_mailer.rb
@@ -1,7 +1,6 @@
 class RetreatAppMailer < ApplicationMailer
 
   def app_submitted(retreat_app)
-    binding.pry
     @retreat_app = retreat_app
     email = retreat_app.user.email
     mail(to: email, subject: 'Application submitted')
